917fdf44613c84ec91501c1d7ca46b7853ff24dd
pawiromitchel <pawiromitchel@users.noreply.github.com>
2026-02-05 08:49:19 +0000

    deploy: 48f999f112f4bfc54822204122977c570bc61c2c

--- constants/chains.js
+++
@@ -1,9 +0,0 @@
-export const CHAINS = Object.freeze({
-    ETHEREUM: 'ethereum',
-    POLYGON: 'polygon',
-    BASE: 'base',
-    ARBITRUM: 'arbitrum',
-    GNOSIS: 'gnosis',
-    OPTIMISM: 'optimism',
-    BNB: 'bnb'
-});
--- routes/ratesRoutes.js
+++
@@ -1,8 +0,0 @@
-// routes/ratesRoutes.js
-import { getRates, getRatesForSpecificChain } from '../controllers/ratesController.js';
-
-export default async function ratesRoutes(fastify, options) {
-    fastify.get('/rates', getRates);
-
-    fastify.get('/rates/:chain', getRatesForSpecificChain);
-}
